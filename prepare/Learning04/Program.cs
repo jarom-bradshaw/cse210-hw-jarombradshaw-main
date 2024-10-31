7acd17cc64d90a36bddc63c848c54c7923655851
jarom-bradshaw <156943555+jarom-bradshaw@users.noreply.github.com>
2024-10-30 23:00:20 -0600

    Finished assignment problem

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Testing the Assignment base class
+        Assignment simpleAssignment = new Assignment("Josh Rodgers", "Subtraction");
+        Console.WriteLine(simpleAssignment.GetSummary());
+
+        // Testing the MathAssignment derived class
+        MathAssignment mathAssignment = new MathAssignment("Martha Mandola", "English", "7.8", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Testing the WritingAssignment derived class
+        WritingAssignment writingAssignment = new WritingAssignment("Jackson Jones", "English", "Proper Syntax Structure");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
